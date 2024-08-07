b19db184e6d2484cb98822997d79c26dba6217f9
Mauricio <mauriciorojascareers@gmail.com>
2024-08-07 08:42:14 -0500

    suspense to fix deploy

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -66,4 +66,12 @@ const EditPrompt = () => {
   );
 };
 
-export default EditPrompt;
+const UpdatePrompt = () => {
+  return (
+    <Suspense>
+      <EditPrompt></EditPrompt>
+    </Suspense>
+  );
+};
+
+export default UpdatePrompt;
